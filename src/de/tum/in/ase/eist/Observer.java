a5e05e370018cb18789d771812d12bf1ed660381
Emi <emi.mano@tum.de>
2022-07-26 18:12:04 +0200

    ab

--- src/de/tum/in/ase/eist/Observer.java
+++ src/de/tum/in/ase/eist/Observer.java
@@ -2,4 +2,5 @@ package de.tum.in.ase.eist;
 
 public interface Observer<T> {
     // TODO realize observer pattern
+    void getUpdate(T changedState);
 }
--- src/de/tum/in/ase/eist/Subject.java
+++ src/de/tum/in/ase/eist/Subject.java
@@ -4,4 +4,21 @@ import java.util.*;
 
 public abstract class Subject<T> {
     // TODO realize observer pattern
+    private final Set<Observer<T>> observers = new HashSet<>();
+
+    public void subscribeObserver(Observer<T> observer) {
+        Objects.requireNonNull(observer);
+        observers.add(observer);
+    }
+
+    public void unsubsribeObserver(Observer<T> observer) {
+        Objects.requireNonNull(observer);
+        observers.remove(observer);
+    }
+    protected void notifyObservers(final T changedState) {
+        for (Observer<T> observer : observers){
+            observer.getUpdate(changedState);
+        }
+
+    }
 }
